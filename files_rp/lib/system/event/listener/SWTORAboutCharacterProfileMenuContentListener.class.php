fda96d23478d01997bc8eebfab189bc23601408a
deraiser <deraiser@gmx.de>
2023-10-02 07:56:34 +0200

    license updated

--- files_rp/lib/system/event/listener/SWTORAboutCharacterProfileMenuContentListener.class.php
+++ files_rp/lib/system/event/listener/SWTORAboutCharacterProfileMenuContentListener.class.php
@@ -10,31 +10,10 @@ use wcf\system\event\listener\IParameterizedEventListener;
 use wcf\system\WCF;
 
 /**
- *  Project:    Raidplaner: Game: Star Wars: The Old Republic
- *  Package:    info.daries.rp.discord
- *  Link:       http://daries.info
- *
- *  Copyright (C) 2018-2022 Daries.info Developer Team
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * Displays information about the character on the about page.
  * 
- * @author      Marco Daries
- * @package     Daries\RP\System\Event\Listener
+ * @author  Marco Daries
+ * @license Raidplaner License <https://daries.dev/licence/raidplaner.txt>
  */
 class SWTORAboutCharacterProfileMenuContentListener implements IParameterizedEventListener
 {
--- files_rp/lib/system/event/listener/SWTORRaidEventControllerListener.class.php
+++ files_rp/lib/system/event/listener/SWTORRaidEventControllerListener.class.php
@@ -15,31 +15,10 @@ use wcf\system\form\builder\field\SingleSelectionFormField;
 use wcf\util\StringUtil;
 
 /**
- *  Project:    Raidplaner: Game: Star Wars: The Old Republic
- *  Package:    info.daries.rp.discord
- *  Link:       http://daries.info
- *
- *  Copyright (C) 2018-2022 Daries.info Developer Team
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published
- *  by the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
- *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * Extended raid event controller
  * 
- * @author      Marco Daries
- * @package     Daries\RP\System\Event\Listener
+ * @author  Marco Daries
+ * @license Raidplaner License <https://daries.dev/licence/raidplaner.txt>
  */
 class SWTORRaidEventControllerListener implements IParameterizedEventListener
 {
